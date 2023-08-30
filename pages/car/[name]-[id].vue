cd5d96bf5e11d3164a9b8eb554f7d97733198391
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-08-30 23:28:08 +0300

    feat: :sparkles: add uniqe dynamic path

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NavBar />
+
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <!-- car detail hero -->
+      <CarDetailHero />
+
+      <!-- car attributes -->
+      <CarDetailAttributes />
+
+      <!-- car description -->
+      <CarDetailDescription />
+
+      <!-- car contact -->
+      <CarDetailContact />
+    </div>
+  </div>
+</template>
