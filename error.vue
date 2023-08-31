c1e5905be8281174255c94d29f7e2706533cabd2
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-08-31 21:20:19 +0300

    feat: :sparkles: add 400 page not found page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="text-9xl">404</h1>
+    <p class="mt-7 text-4xl">Page not found</p>
+    <button
+      class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white"
+      @click="handleReturn"
+    >
+      Go Back
+    </button>
+  </div>
+</template>
+
+<script setup>
+const handleReturn = () => {
+  navigateTo("/");
+};
+</script>
