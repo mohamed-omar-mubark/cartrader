01d95f985dcc92d6deedf301b9b31b86ffa4f0c1
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-09-05 02:08:04 +0300

    feat: :sparkles: implementing google OAuth

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+  });
+
+  if (error) {
+    console.log(error);
+  }
+};
+</script>
+
+<template>
+  <div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">Login</h1>
+    <button @click="login" class="bg-red-400 p-3 rounded text-white font-bold">
+      Login with Google
+    </button>
+  </div>
+</template>
