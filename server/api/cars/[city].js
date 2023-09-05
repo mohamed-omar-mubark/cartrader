4c1b5eb552430264066a3594c3e2847d2cd8f2ec
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-09-06 01:56:20 +0300

    feat: :sparkles: extracting the query params

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -2,12 +2,27 @@ import cars from "@/data/cars.json";
 
 export default defineEventHandler((event) => {
   const { city } = event.context.params;
+  const { make, minPrice, maxPrice } = getQuery(event);
 
   let filteredCars = cars;
 
   filteredCars = filteredCars.filter(
     (car) => car.city.toLowerCase() === city.toLowerCase()
   );
+
+  if (make) {
+    filteredCars = filteredCars.filter(
+      (car) => car.make.toLowerCase() === make.toLowerCase()
+    );
+  }
+
+  if (minPrice) {
+    filteredCars = filteredCars.filter((car) => car.price >= minPrice);
+  }
+
+  if (maxPrice) {
+    filteredCars = filteredCars.filter((car) => car.price <= maxPrice);
+  }
 
   return filteredCars;
 });
